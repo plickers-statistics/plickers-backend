af3d46018c6a113bae81a539c77f6979ffa6bc87
nvd05 <nvd05@bk.ru>
2024-11-13 11:23:43 +0500

    Create tables.sql

---
+++ database/tables.sql
@@ -0,0 +1,60 @@
+
+-- пользователи
+CREATE TABLE `users`
+(
+    `identifier` INTEGER NOT NULL PRIMARY KEY,
+
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    `changed_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+
+    `name` TEXT NOT NULL
+);
+
+-- история подключений
+CREATE TABLE `connection_history`
+(
+    `identifier` INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+
+    `connected_at`    TIMESTAMP NOT NULL,
+    `disconnected_at` TIMESTAMP NOT NULL,
+
+    `ip_address`      TEXT    NOT NULL,
+    `user_identifier` INTEGER     NULL REFERENCES `users` (`identifier`)
+);
+
+-- вопросы
+CREATE TABLE `questions`
+(
+    `identifier` INTEGER NOT NULL PRIMARY KEY,
+
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    `changed_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+
+    `formulation_html` TEXT NOT NULL
+);
+
+-- варианты ответов
+CREATE TABLE `options`
+(
+    `identifier` INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    `changed_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+
+    `question_identifier` INTEGER NOT NULL REFERENCES `questions` (`identifier`),
+    `option_identifier`   INTEGER NOT NULL,
+    `formulation_html`    TEXT    NOT NULL
+);
+
+-- ответы пользователей
+CREATE TABLE `answers`
+(
+    `identifier` INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    `changed_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+
+    `user_identifier`     INTEGER NOT NULL REFERENCES `users`     (`identifier`),
+    `question_identifier` INTEGER NOT NULL REFERENCES `questions` (`identifier`),
+    `option_identifier`   INTEGER NOT NULL
+);
