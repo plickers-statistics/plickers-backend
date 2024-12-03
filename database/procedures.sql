0c8f0d13e572dfd362ed2fcbe56e96a1bb1bbad0
nvd05 <nvd05@bk.ru>
2024-12-03 23:41:41 +0500

    Update procedures.sql

--- database/procedures.sql
+++ database/procedures.sql
@@ -6,24 +6,17 @@ CREATE OR REPLACE PROCEDURE REPLACE_IF_EXISTS_ELSE_ADD_CLASS_ROOM
     IN parameter_teacher_name TEXT
 )
 BEGIN
-    SELECT (@name := `name`), (@teacher_name := `teacher_name`) FROM `classes` WHERE `identifier` = parameter_identifier LIMIT 1;
+    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
+        ROLLBACK;
 
-    IF (@name IS NULL)
-    THEN
-        INSERT INTO `classes`
-            (`identifier`, `name`, `teacher_name`)
-        VALUES
-            (parameter_identifier, parameter_name, parameter_teacher_name);
-    ELSE
-        IF (@name != parameter_name OR @teacher_name != parameter_teacher_name)
-        THEN
-            UPDATE `classes` SET
-                `changed_at`   = CURRENT_TIMESTAMP(),
-                `name`         = parameter_name,
-                `teacher_name` = parameter_teacher_name
-            WHERE `identifier` = parameter_identifier;
-        END IF;
-    END IF;
+    START TRANSACTION;
+
+    INSERT INTO `classes` (`identifier`, `name`, `teacher_name`)
+    VALUES (parameter_identifier, parameter_name, parameter_teacher_name)
+    ON DUPLICATE KEY UPDATE
+        `name`         = VALUES(`name`),
+        `teacher_name` = VALUES(`teacher_name`),
+        `changed_at`   = CURRENT_TIMESTAMP();
 
     COMMIT;
 END;
@@ -35,23 +28,16 @@ CREATE OR REPLACE PROCEDURE REPLACE_IF_EXISTS_ELSE_ADD_STUDENT
     IN parameter_class_identifier VARCHAR(24)
 )
 BEGIN
-    SET @first_name := (SELECT `first_name` FROM `students_in_classes` WHERE `identifier` = parameter_identifier LIMIT 1);
+    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
+        ROLLBACK;
 
-    IF (@first_name IS NULL)
-    THEN
-        INSERT INTO `students_in_classes`
-            (`identifier`, `first_name`, `class_identifier`)
-        VALUES
-            (parameter_identifier, parameter_first_name, parameter_class_identifier);
-    ELSE
-        IF (@first_name != parameter_first_name)
-        THEN
-            UPDATE `students_in_classes` SET
-                `changed_at` = CURRENT_TIMESTAMP(),
-                `first_name` = parameter_first_name
-            WHERE `identifier` = parameter_identifier;
-        END IF;
-    END IF;
+    START TRANSACTION;
+
+    INSERT INTO `students_in_classes` (`identifier`, `first_name`, `class_identifier`)
+    VALUES (parameter_identifier, parameter_first_name, parameter_class_identifier)
+    ON DUPLICATE KEY UPDATE
+        `first_name` = VALUES(`first_name`),
+        `changed_at` = CURRENT_TIMESTAMP();
 
     COMMIT;
 END;
