4c0918a5df4eec6fa4c32cbe5aa3d5561658df4a
nvd05 <nvd05@bk.ru>
2024-12-09 16:56:56 +0500

    added StaticFiles

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 
 from fastapi import FastAPI
+from starlette.staticfiles import StaticFiles
 
 from src.lifespan import lifespan
 from src.routers.api import api_router
@@ -7,3 +8,4 @@ from src.routers.api import api_router
 
 app = FastAPI(lifespan = lifespan)
 app.include_router(api_router)
+app.mount('/', StaticFiles(directory='public'), name='public')
